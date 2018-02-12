29602cf8c97882492a930685986d2de634d9da47
Q-Zhao <qzhao@wpi.edu>
2018-02-12 00:07:41 -0500

    sample code for aws api gateway/lambda function/s3 bucket/rds database.

--- src/main/java/edu/wpi/cs/cloudcomputing/controller/BookManager.java
+++ src/main/java/edu/wpi/cs/cloudcomputing/controller/BookManager.java
@@ -1,6 +1,7 @@
 package edu.wpi.cs.cloudcomputing.controller;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gson.Gson;
@@ -15,27 +16,58 @@ public class BookManager {
 
 		BookDAO bookDAO = new BookDAO();
 		List<Book> allBooks = bookDAO.getAllBooks();
-		
-		/*	demo without connecting to DB
-		List<Book> allBooks = new ArrayList<>();
-		allBooks.add(this.createABook());
-		 */
-
 		Gson gson = new GsonBuilder().create();
 		Type listType = new TypeToken<List<Book>>(){}.getType();
 		return gson.toJson(allBooks, listType);
 
 	}
+	
+	public String getAllCachedBooks() throws Exception {
+		List<Book> allBooks = createLibrary();
+		Gson gson = new GsonBuilder().create();
+		Type listType = new TypeToken<List<Book>>(){}.getType();
+		return gson.toJson(allBooks, listType);
+	}
+	
+	public String getCachedBooks(String ISBN) throws Exception {
+		List<Book> allBooks = createLibrary();
+		for (Book book : allBooks) {
+			if (book.getISBN().equals(ISBN)) {
+				Gson gson = new GsonBuilder().create();
+				return gson.toJson(book);
+			}
+		}
+		throw new Exception("Book Not Found");
+	}
+	
+	private List<Book> createLibrary() {
+		List<Book> library = new ArrayList<>();
+		library.add(this.createBookOne());
+		library.add(this.createBookTwo());
+		return library;
+	}
 
-    private Book createABook(){
+    private Book createBookOne(){
     	Book demoBook = new Book();
-        demoBook.setTitle("DemoBookTitle");
-        demoBook.setAuthor("DemoAuthor");
-        demoBook.setDescription("This book is about a demo book of this review system");
-        demoBook.setGenre("DemoGenre");
-        demoBook.setISBN("DemoISBN");
+        demoBook.setTitle("Animals in Translation: Using the Mysteries of Autism to Decode Animal Behavior");
+        demoBook.setAuthor("Temple Grandin, Catherine Johnson");
+        demoBook.setDescription("In this exciting new e edition, Temple Grandin returns to her groundbreaking work, Animals in Translation, to address the last ten years of developments in behavioral research, animal welfare, and farming regulations. Originally published in 2005, Animals in Translation received unanimous critical praise and was a bestseller in both hardcover and paperback, and Grandin’s Q&A updates this classic text with the most current scientific research.");
+        demoBook.setGenre("Animal");
+        demoBook.setISBN("0156031442");
         demoBook.setScore((float) 4.5);
-        demoBook.setImageUrl("");
+        demoBook.setImageUrl("http://www.templegrandin.com/_images/_books/AnimalsAutismTemple.png");
+        return demoBook;
+    }
+    
+    private Book createBookTwo(){
+    	Book demoBook = new Book();
+        demoBook.setTitle("Organic Chemistry As a Second Language: First Semester Topics");
+        demoBook.setAuthor("David R. Klein");
+        demoBook.setDescription("Readers continue to turn to Klein's Organic Chemistry as a Second Language: First Semester Topics, 4th Edition because it enables them to better understand fundamental principles, solve problems, and focus on what they need to know to succeed. This edition explores the major principles in the field and explains why they are relevant. It is written in a way that clearly shows the patterns in organic chemistry so that readers can gain a deeper conceptual understanding of the material. Topics are presented clearly in an accessible writing style along with numerous hands-on problem solving exercises.");
+        demoBook.setGenre("Science");
+        demoBook.setISBN("1119110661");
+        demoBook.setScore((float) 4.5);
+        demoBook.setImageUrl("https://prodimage.images-bn.com/pimages/9781119110668_p0_v2_s600x595.jpg");
         return demoBook;
     }
 }
