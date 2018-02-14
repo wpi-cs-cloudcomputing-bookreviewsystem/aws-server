cf673f86ed4077914d06dd4ad02ee3ec661083ba
echomelodylwh <wli6@wpi.edu>
2018-02-13 21:33:15 -0500

    add cognito

--- src/main/java/edu/wpi/cs/cloudcomputing/utils/Common.java
+++ src/main/java/edu/wpi/cs/cloudcomputing/utils/Common.java
@@ -1,5 +1,16 @@
 package edu.wpi.cs.cloudcomputing.utils;
 
 public class Common {
+    public static final String access_key = "AKIAIUQME7WR2XPBDN7Q";
+    public static final String secret_key = "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";
+    public static final String cognito_pool_id = "us-east-1_xftf9ALpN";
+    public static final String cognito_client_id = "7g3a99d1kfmg8lcfn86qljgt59";
+
+    public final static String NOT_LOGGED_IN = "NOT LOGGED IN";
+    public final static String LOGGED_IN = "LOGGED IN";
+    public final static String INVALID_REQUEST = "INVALID REQUEST";
+    public final static String NO_SUCH_USER = "NO SUCH USER";
+    public final static String USER_ALREADY_EXISTS = "USER ALREADY EXISTS";
+    public final static String USER_CREATED = "USER CREATED";
 
 }
