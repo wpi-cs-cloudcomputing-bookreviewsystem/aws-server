ddf8cf07a680ce5a69abef83c75ff2a8671deaa7
echomelodylwh <wli6@wpi.edu>
2018-02-13 21:37:01 -0500

    delete

--- src/main/java/edu/wpi/cs/cloudcomputing/utils/Common.java
+++ src/main/java/edu/wpi/cs/cloudcomputing/utils/Common.java
@@ -1,10 +1,10 @@
 package edu.wpi.cs.cloudcomputing.utils;
 
 public class Common {
-    public static final String access_key = "AKIAIUQME7WR2XPBDN7Q";
-    public static final String secret_key = "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";
-    public static final String cognito_pool_id = "us-east-1_xftf9ALpN";
-    public static final String cognito_client_id = "7g3a99d1kfmg8lcfn86qljgt59";
+    public static final String access_key = "";
+    public static final String secret_key = "";
+    public static final String cognito_pool_id = "";
+    public static final String cognito_client_id = "";
 
     public final static String NOT_LOGGED_IN = "NOT LOGGED IN";
     public final static String LOGGED_IN = "LOGGED IN";
